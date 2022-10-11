4bab5515c404b028df60ba48634d768376b32696
Jakub Czerwik <jakub.czerwik@kcl.ac.uk>
2022-10-11 16:55:35 +0100

    Add code generating a selected number of prime no's to a list

--- primes.py
+++ primes.py
@@ -3,4 +3,16 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes>0:
+        currentprime =2
+        while number_of_primes>len(list):
+            multiples=False
+            for i in range(currentprime+1):
+                if i>1 and currentprime%i==0 and not currentprime==i:
+                    multiples=True
+                if currentprime==i and multiples==False:
+                    list.append(currentprime)
+            currentprime=currentprime+1
+    else:
+        raise ValueError(f'The number of primes you inserted : {number_of_primes} cannot be smaller than 1')
     return list
